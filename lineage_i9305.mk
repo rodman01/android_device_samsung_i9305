ac51d20c4ca16a14a26d12b2de840e6611e73281
rodman01 <email4joern@arcor.de>
2019-07-24 18:17:04 +0200

    not sure whether this is of any help and effect
    Change-Id: I8a086900eaf9b11513561b0888596d47726d9c3a

--- lineage_i9305.mk
+++ lineage_i9305.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -22,10 +23,10 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9305
 PRODUCT_MANUFACTURER := samsung
 
-# Set build fingerprint / ID / Product Name etc.
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-		PRODUCT_NAME=m3xx \
-		TARGET_DEVICE=m3 \
-		PRIVATE_BUILD_DESC="m3xx-user 4.4.4 KTU84P I9305XXUFQG1 release-keys"
-
-BUILD_FINGERPRINT := samsung/m3xx/m3:4.4.4/KTU84P/I9305XXUFQG1:user/release-keys
+	PRODUCT_NAME=m3xx \
+	TARGET_DEVICE=m3 \
+	PRIVATE_BUILD_DESC="m3xx-user 4.4.4 KTU84P I9305XXUFPB1 release-keys"
+	
+BUILD_FINGERPRINT="samsung/m3xx/m3:4.4.4/KTU84P/I9305XXUFPB1:user/release-keys
